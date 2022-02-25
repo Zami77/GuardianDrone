a2799e5a7186a1ba16384c6c01d42996ca33edc5
Daniel Lungaro <chunk_77@hotmail.com>
2022-02-25 04:07:22 -0800

    implemented enemy hit blink

--- src/Entities/Enemies/BasicEnemy.cs
+++ src/Entities/Enemies/BasicEnemy.cs
@@ -4,28 +4,32 @@ using System;
 public class BasicEnemy : KinematicBody2D, IDamageable
 {
 	const int BASIC_ENEMY_MOVE_SPEED = 140;
-	int enemyHealth = 20;
+	const string TAKE_DAMAGE_ANIM = "TakeDamage";
+	int enemyHealth = 50;
 	KinematicBody2D guarded;
+	AnimationPlayer animationPlayer;
 
 	public override void _Ready()
 	{
 		guarded = GetNode<KinematicBody2D>("../GuardedPath2D/GuardedPathFollow2D/Guarded");
+		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
     public void TakeDamage(int dmg)
     {
-        enemyHealth -= dmg;
-		GD.Print($"Super health:{enemyHealth}");
+    	enemyHealth = Math.Max(0, enemyHealth - dmg);
+		animationPlayer.Play(TAKE_DAMAGE_ANIM);
     }
 
-	private bool isDead() => enemyHealth <= 0;
+	private bool isDead() => enemyHealth == 0;
+
     public override void _Process(float delta)
 	{
 		if (isDead())
 		{
 			QueueFree();
 		}
-		
+
 		var velocity = Position.DirectionTo(guarded.GlobalPosition) * BASIC_ENEMY_MOVE_SPEED * delta;
 		GlobalPosition += velocity;
 	}
