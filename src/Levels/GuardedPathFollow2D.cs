1c23d476a0a62a1fe901056d1e9694f72fe6a278
Daniel Lungaro <chunk_77@hotmail.com>
2022-02-25 09:04:50 -0800

    Update GuardedPathFollow2D.cs

--- src/Levels/GuardedPathFollow2D.cs
+++ src/Levels/GuardedPathFollow2D.cs
@@ -4,9 +4,11 @@ using System;
 public class GuardedPathFollow2D : PathFollow2D
 {
 	const int GUARDED_MOVE_SPEED = 150;
+	KinematicBody2D guarded;
 	public override void _Ready()
 	{
-		
+		// TODO: get Guarded Node to see if dead to stop movement
+		// guarded = GetNode<KinematicBody2D>("./Guarded");
 	}
 
 	public override void _Process(float delta)
